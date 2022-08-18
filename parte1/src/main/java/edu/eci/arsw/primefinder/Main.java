2fb9bed6b5021b8eed86652455f0888a6ff6db46
unknown <jeison.barreto@mail.escuelaing.edu.co>
2022-08-18 09:02:54 -0500

    Parte I

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,13 +1,24 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+	/*
 		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
+
+		pft.start();*/
+
+
+		ArrayList<PrimeFinderThread> pft = new ArrayList<>();
+		int a = 0;
+		for (int i = 1 ; i <= 3 ; i++){
+			pft.add(new PrimeFinderThread(a, 10000000*i));
+			a = 10000000*i;
+
+		}
+		for (PrimeFinderThread spft : pft) spft.start();
 		
 	}
 	
